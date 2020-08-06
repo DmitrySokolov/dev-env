7ac795351e7dec29bd8641237eece6da7a3a8fdf
Dmitry Sokolov <mr.dmitry.sokolov@gmail.com>
2020-08-06 22:31:59 +0300

    Added 'JDK 8' meta package.

---
+++ packages/JDK__8.ps1
@@ -0,0 +1,7 @@
+Add-PackageInfo `
+    -Name "JDK" `
+    -Description "Java Development Kit v8 [LTS]" `
+    -Version "8" `
+    -Platform "x86_64" `
+    -DependsOn @("JDK__8.0.265.01") `
+    -IsMetaPackage $true
